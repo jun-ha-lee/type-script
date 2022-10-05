375cf5e7257c2557ececb837c1e4ce381b07a8d8
jun-ha-lee <hello.git.oil_bird@naver.com>
2022-10-05 12:31:21 +0900

    타입스크립트

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const darkTheme = {
+//   textColor: "whitesmoke",
+//   backgroundColor: "#111",
+// };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const lightTheme = {
+//   textColor: "#111",
+//   backgroundColor: "whitesmoke",
+// };
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// createRoot가 typescript에서는 안댐
+
+ReactDOM.render(
+  // <ThemeProvider theme={darkTheme}>
+  //   <App />
+  // </ThemeProvider>,
+  <App />,
+  document.getElementById("root")
+);
